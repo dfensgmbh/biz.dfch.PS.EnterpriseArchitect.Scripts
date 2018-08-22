8f511fbad88cd292a3f703985e182b0bec944c42
rufer7 <rufermarc@gmail.com>
2018-08-22 10:41:02 +0200

    Script for retrieving diagrams added

---
+++ src/Get-Diagram.Tests.ps1
@@ -0,0 +1,148 @@
+#Requires -Modules @{ ModuleName = 'biz.dfch.PS.Pester.Assertions'; ModuleVersion = '1.1.1' }
+
+$here = Split-Path -Parent $MyInvocation.MyCommand.Path;
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".");
+
+Describe "Get-Diagram" {
+	
+	. "$here\$sut";
+	. "$here\Open-EaRepository.ps1";
+	. "$here\Get-Model.ps1";
+	. "$here\Get-Package.ps1";
+	. "$here\Close-EaRepository.ps1";
+	
+	Context "Get-Diagram-ValidationTests" {
+		It "Warmup" -Test {
+			
+			# Arrange
+			
+			# Act
+			
+			# Assert
+			$true | Should Be $true;
+		}
+		
+		It "ThrowsParameterBindingValidationExceptionWhenInvokingWithNullEaModelOrPackage" {
+			
+			# Arrange
+
+			# Act
+			{ Get-Diagram -EaModelOrPackage $null; } | Should ThrowException 'ParameterBindingValidationException';
+
+			# Assert
+		}
+		
+		It "ThrowsParameterBindingValidationExceptionWhenInvokingWithNullName" {
+			
+			# Arrange
+			$eaModel = New-Object -ComObject Scripting.Dictionary;
+			
+			# Act
+			{ Get-Diagram -EaModelOrPackage $eaModel -Name $null; } | Should ThrowException 'ParameterBindingValidationException';
+			
+			#Assert
+		}
+		
+		It "ThrowsParameterBindingValidationExceptionWhenInvokingWithEmptyName" {
+			
+			# Arrange
+			$eaModel = New-Object -ComObject Scripting.Dictionary;
+			
+			# Act
+			{ Get-Diagram -EaModelOrPackage $eaModel -Name ""; } | Should ThrowException 'ParameterBindingValidationException';
+			
+			#Assert
+		}
+	}
+	
+	Context "Get-Diagram-PositiveTests" {
+		
+		$pathToEaRepository = "$here\SampleModel.eapx";
+		
+		BeforeEach {
+			$eaRepository = Open-EaRepository -Path $pathToEaRepository;
+			$eaModel = Get-Model $eaRepository;
+			$eaPackage = Get-Package $eaModel -Recurse -Name "1";
+		}
+		
+		It "RetrievesAndReturnsListOfAvailableDiagramsOfSpecifiedModelWhenInvokingWithValidEaModel" {
+			
+			# Arrange
+			
+			# Act
+			$result = Get-Diagram $eaModel;
+			
+			# Assert
+			$result | Should Be $null;
+		}
+		
+		It "RetrievesAndReturnsListOfAvailableDiagramsOfSpecifiedPackageWhenInvokingWithValidEaPackage" {
+			
+			# Arrange
+			
+			# Act
+			$result = Get-Diagram $eaPackage;
+			
+			# Assert
+			$result | Should Not Be $null;
+			$result.Count | Should Be 2;
+		}
+		
+		It "RetrievesAndReturnsDiagramByNameOfSpecifiedPackageWhenInvokingWithValidEaPackageAndName" {
+			
+			# Arrange
+			$diagramName = "class-diagram";
+			
+			# Act
+			$result = Get-Diagram $eaPackage -Name $diagramName;
+			
+			# Assert
+			$result | Should Not Be $null;
+			$result.Name | Should Be $diagramName;
+		}
+		
+		It "RetrievesDiagramsOfSpecifiedModelRecursivelyAndReturnsThemWhenInvokingWithValidEaModelAndRecurseSwitch" {
+			
+			# Arrange
+			
+			# Act
+			$result = Get-Diagram $eaModel -Recurse;
+			
+			# Assert
+			$result | Should Not Be $null;
+			$result.Count | Should Be 5;
+		}
+		
+		It "RetrievesDiagramsOfSpecifiedPackageRecursivelyAndReturnsThemWhenInvokingWithValidEaPackageAndRecurseSwitch" {
+			
+			# Arrange
+			
+			# Act
+			$result = Get-Diagram $eaPackage -Recurse;
+			
+			# Assert
+			$result | Should Not Be $null;
+			$result.Count | Should Be 2;
+		}
+
+		AfterEach {
+			$null = Close-EaRepository $eaRepository;
+		}
+	}
+}
+
+#
+# Copyright 2018 d-fens GmbH
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
---
+++ src/Get-Diagram.ps1
@@ -0,0 +1,171 @@
+#Requires -Modules @{ ModuleName = 'biz.dfch.PS.System.Logging'; ModuleVersion = '1.4.1' }
+
+Function Get-Diagram {
+<#
+.SYNOPSIS
+
+Retrieves one or more EA diagram objects from the specified EA model or EA package.
+
+.DESCRIPTION
+
+Retrieves one or more EA diagram objects from the specified EA model or EA package.
+
+The EA model or EA package has to be provided as input by either positional or named parameter.
+
+.EXAMPLE
+
+Get-Diagram $eaModel
+
+EA model is passed as a positional parameter to the Cmdlet.
+
+.EXAMPLE
+
+Get-Diagram $eaPackage
+
+EA package is passed as a positional parameter to the Cmdlet.
+
+.LINK
+
+GitHub Repository: https://github.com/dfensgmbh/biz.dfch.PS.EnterpriseArchitect.Scripts
+
+#>
+
+[CmdletBinding(
+    SupportsShouldProcess = $true
+	,
+    ConfirmImpact = "Low"
+	,
+	DefaultParameterSetName = 'list'
+)]
+PARAM
+(
+	[ValidateNotNull()]
+	[Parameter(Mandatory = $true, Position = 0)]
+	$EaModelOrPackage
+	,
+	# Full name or part of it, for the EA diagram you want to search - this is not case sensitive
+	[Parameter(Mandatory = $false, ParameterSetName = 'searchByName')]
+	[ValidateNotNullOrEmpty()]
+	[String] $Name = $null
+	,
+	# Lists all available EA diagrams of the EA model/package
+	[Parameter(Mandatory = $false, ParameterSetName = 'list')]
+	[Switch] $ListAvailable
+	,
+	# Considers recursively all available EA diagrams of the EA model/package
+	[Parameter(Mandatory = $false)]
+	[Switch] $Recurse
+)
+
+BEGIN
+{
+	trap { Log-Exception $_; break; }
+}
+
+PROCESS
+{
+	trap { Log-Exception $_; break; }
+	
+	function GetPackagesOfPackage($Package)
+	{
+		$temp = [System.Collections.ArrayList]::new();
+
+		if ($Package.Packages -ne $null)
+		{
+			foreach($pkg in $Package.Packages)
+			{
+				$null = $temp.Add($pkg);
+				$temp2 = GetPackagesOfPackage $pkg;
+				if ($temp2 -ne $null)
+				{
+					if ($temp2.Count -gt 0)
+					{
+						$null = $temp.AddRange($temp2);
+					}
+					else 
+					{
+						$null = $temp.Add($temp2);
+					}
+				}
+			}
+		}
+		
+		return $temp;
+	}
+
+	if($Recurse)
+	{
+		$packages = [System.Collections.ArrayList]::new();
+		
+		foreach($package in $EaModelOrPackage.Packages)
+		{
+			$null = $packages.Add($package);
+			$temp = GetPackagesOfPackage $package;
+			if ($temp -ne $null)
+			{
+				if ($temp.Count -gt 0)
+				{
+					$null = $packages.AddRange($temp);
+				}
+				else
+				{
+					$null = $packages.Add($temp);
+				}
+			}
+		}
+		
+		$diagrams = [System.Collections.ArrayList]::new();
+		
+		foreach($pkg in $packages)
+		{
+			if ($pkg.Diagrams.Count -gt 0)
+			{
+				$null = $diagrams.AddRange($pkg.Diagrams);
+			}
+			else
+			{
+				$null = $diagrams.Add($pkg.Diagrams);
+			}
+		}
+	}
+	else
+	{
+		$diagrams = $EaModelOrPackage.Diagrams;
+	}
+	
+	if($PSCmdlet.ParameterSetName -eq 'list')
+	{
+		$result = $diagrams;
+	}
+	else
+	{
+		If ($PSCmdlet.ParameterSetName -eq 'SearchByName')
+		{
+			$result = $diagrams |? Name -match $Name;
+		}
+	}
+	
+	$OutputParameter = $result;
+}
+
+END
+{	
+	return $OutputParameter;
+}
+}
+
+#
+# Copyright 2018 d-fens GmbH
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
